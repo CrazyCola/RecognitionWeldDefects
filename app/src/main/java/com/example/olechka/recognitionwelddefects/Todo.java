2dde81247207b7d679dd691b10f1c9c2e01a2f50
CrazyCola <Vo777Rainbow@mail.ru>
2017-05-24 09:17:44 +0300

    DB

---
+++ app/src/main/java/com/example/olechka/recognitionwelddefects/Todo.java
@@ -0,0 +1,30 @@
+//package com.example.olechka.recognitionwelddefects;
+//
+//import com.orm.SugarRecord;
+//
+///**
+// * Created by Olechka on 22.05.2017.
+// */
+//public class Todo extends SugarRecord<Todo> {
+//    String todo;
+//
+//    public Todo(){
+//    }
+//
+//    public Todo (String todo){
+//        this.todo = todo;
+//    }
+//
+//    public String getTodo(){
+//        return todo;
+//    }
+//    public void  setTodo(String todo){
+//        this.todo=todo;
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return todo;
+//    }
+//}
+//
